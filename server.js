a7982b494a832b4633a8aaaefb379a84bb2da57c
Eric Schweitzer <eric_schweitzer@yahoo.com>
2022-08-04 14:07:35 -0500

    added heroku and server.js file

---
+++ server.js
@@ -0,0 +1,8 @@
+// added this file in root was not in starter code 
+const express = require('express');
+const app = express();
+
+
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+  });
